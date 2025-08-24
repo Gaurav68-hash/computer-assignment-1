4c49def0af6c848c1c3756fafcf20c0896f9663d
Gaurav68-hash <gauravpchaudhary0942@gmail.com>
2025-08-24 22:27:04 +0530

    Add files via upload

---
+++ multiply two numbers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+ printf("Enter two numbers for Multiplication :");
+ int a,b,c ;
+ scanf("%d %d", &a, &b);
+ c = a*b;
+ printf("The Multiplication is %d", c);
+ return 0;
+}
