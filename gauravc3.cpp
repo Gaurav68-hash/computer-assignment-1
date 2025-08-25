713dab028fae9fe12b941bbe287c4a45e291482b
Gaurav68-hash <gauravpchaudhary0942@gmail.com>
2025-08-25 10:41:36 +0530

    Add files via upload

---
+++ gauravc3.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdio.h>
+int main()
+ {
+
+   int i,n;
+    i=0;
+    printf("Enter a value.");
+    scanf("%d",&n);  i=0;
+    do
+{
+    i ++;
+    printf("%d\n",i);
+
+}while (i<n);
+ }
